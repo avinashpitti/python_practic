205301be3fd06070093880b4ef9e89f6fb0d3b26
avinashpitti <avinash87129@gmail.com>
2025-12-22 11:57:39 +0530

    practice int typecasting

--- practice2.py
+++ practice2.py
@@ -1,51 +1,45 @@
-#arithmetic operators works with integers,floats,complex and boolean values only
+# Assignment operators are used to assign values to variables
+# = is the basic assignment operator
+# Always write operator first equals next
 
-a=7
-b=4
-c=True
-d=False
+# +=,-=,*=,/=,//=,%=,*=,^=,&=,|=,<<=,>>=
 
+#Integers and floats are always stored as different objects in memory,
+#even if their values look the same.# a=10 b=10.0
+a=10
 
-print("The value of a+b is",a+b)
-print("The value of a-b is",a-b)
+a=int(a)
+print(a)
+print(type(a))
+print("**************")
 
-print("The value of a*b is",a*b)
-print("The value of a/b is",a/b)
-
-print("The value of a//b is",a//b)
-print("The value of a**b is",a**b)
-print("The value of a%b is",a%b)
-
-print("The value of a+c is",a+c)
-print("The value of b+d is",b+d)
-
-print("The value of a**d is",a**d)
-print("The value of a**c is",a**c)
-
-e=3+8j
-
-print("The value of d+e is",d+e)
-print("The value of c+e is",c+e)
-
-f="avinash "
-
-# print("The product of a*f is",(f+' ')*a)
+# a=float(a)
+# print(a)
+# print(type(a))
+# print("**************")
 
 
 
-print("The product of f* a is",(f*a).strip())
-
-print(True/True)
-# print(True/False) # zero division error
-
-print(False/True)
+a=str(a)
+print(a)
+print(type(a))
+print("**************")
 
 
-print(0.1+0.2)
 
-quotient,remainder=divmod(18,4)
-print("quotient",quotient)
-print("remainder",remainder)
+# a=bool(a)
+# print(a)
+# print(type(a))
+# print("**************")
 
+# a=complex(a)
+# print(a)
+# print(type(a))
+# print("****************")
+
+
+a=list(a)
+print(a)
+print(list(a))
 
 
