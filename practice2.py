2fb6cc7cc0ef5b775597ed10875b3bd123c5ff64
avinashpitti <avinash87129@gmail.com>
2025-12-22 11:03:21 +0530

    practice arithmetic operators

--- practice2.py
+++ practice2.py
@@ -1,7 +1,51 @@
-text=("amar",'bhanu','cindy','deng','ester',32,56.8)
-print(text)
-print(len(text))
-print(type(text))
+#arithmetic operators works with integers,floats,complex and boolean values only
 
-text[0]="avinash"
-print(text)
+a=7
+b=4
+c=True
+d=False
+
+
+print("The value of a+b is",a+b)
+print("The value of a-b is",a-b)
+
+print("The value of a*b is",a*b)
+print("The value of a/b is",a/b)
+
+print("The value of a//b is",a//b)
+print("The value of a**b is",a**b)
+print("The value of a%b is",a%b)
+
+print("The value of a+c is",a+c)
+print("The value of b+d is",b+d)
+
+print("The value of a**d is",a**d)
+print("The value of a**c is",a**c)
+
+e=3+8j
+
+print("The value of d+e is",d+e)
+print("The value of c+e is",c+e)
+
+f="avinash "
+
+# print("The product of a*f is",(f+' ')*a)
+
+
+
+print("The product of f* a is",(f*a).strip())
+
+print(True/True)
+# print(True/False) # zero division error
+
+print(False/True)
+
+
+print(0.1+0.2)
+
+quotient,remainder=divmod(18,4)
+print("quotient",quotient)
+print("remainder",remainder)
+
+
+
