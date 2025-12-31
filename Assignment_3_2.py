dd913d4f07a35a9d4609f3039544c34d336ae1f1
avinashpitti <avinash87129@gmail.com>
2025-12-31 17:43:45 +0530

    few problems practiced

--- Assignment_3_2.py
+++ Assignment_3_2.py
@@ -1,6 +1,9 @@
 # 1. Print given numbers of '*' or stars in a row
 
-print('***********')
+# n=int(input("Enter number of starts you want to print: "))
+print(('*')*12)
+
+print('-------------1---------------')
 
 # 2. Print square pattern with 'A' symbols.
 # A A A
@@ -13,9 +16,8 @@ for i in range(3):
 
     print()
 
-for i in range(3):
-    print('A B A')
 
+print('------------2----------------')
 
 # 3. Print square pattern with '*' symbols
 
@@ -24,6 +26,7 @@ for i in range(4):
         print('*',end=' ')
     print()
     
+print('-------------3---------------')
 
 # 4. Print square pattern with given number/digit.
 # 5 5 5 5 5
@@ -38,6 +41,8 @@ for i in range(5):
         print('5',end=' ')
     print()
 
+print('------------4----------------')
+
 
 # 5. Print square pattern with a given fixed digit in every row.
 # 1 1 1 1
@@ -49,6 +54,9 @@ for i in range(1,5):
     for j in range(4):
         print(i,end=' ')
     print(i)
+
+print('------------5----------------')
+
 
 #6. Print square pattern with given digit in descending order.
 # 5 5 5 5 5
@@ -62,3 +70,75 @@ for i in range(5):
         print(i,end=' ')
     print()
 
+print('-----------6-----------------')
+
+
+# #7. Print Right Angle Triangle pattern with '*' symbol.
+# *
+# * *
+# * * *
+# * * * *
+
+# count=0
+# for i in range(4):
+#     for j in range(1):
+#         count+=1
+#     print('*'* count)
+
+for i in range(1,5):
+    print('* '*i)
+
+print('-----------7-----------------')
+
+# #8. Print Right AngleTriangle pattern with 'A' Symbol.
+# A
+# A A
+# A A A
+# A A A A
+
+
+for i in range(1,5):
+    print('A '*i)
+
+print('-----------8-----------------')
+
+
+# #9. Print Reverse Right Angle Triangle Pattern with ‘*’ Symbol
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+
+for i in range(5,0,-1):
+    print('*'*i)
+
+print('-----------9-----------------')
+
+# #10.Print Right Angle Triangle with Fixed Alphabet Symbol
+# A
+# B B
+# C C C
+# D D D D
+
+ch = 'A'# if we give space after A,TypeError: ord() expected a character, but string of length 2 found
+
+for i in range(1,5):
+    print((ch+' ')*i)
+    ch=chr(ord(ch)+1)
+
+print('-----------10-----------------')
+
+
+# 11.Print right angle triangle with fixed digit symbol in every row
+# 1
+# 2 2
+# 3 3 3
+# 4 4 4 4
+
+n=4
+
+for i in range(1,n+1):
+    for j in range(i):
+        print(i,end=' ')
+    print()
