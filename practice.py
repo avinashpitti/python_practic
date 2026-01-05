fa1dda37c196dcb771aad82a826d7249c106fcf6
avinashpitti <avinash87129@gmail.com>
2026-01-05 13:15:28 +0530

    types of arguments in function

--- practice.py
+++ practice.py
@@ -1,18 +1,38 @@
-def my_func():
-    print("hello python")
-my_func()
-my_func()
-my_func()
+# In positional arguments the output changes based on positions
+def sub(a,b):
+    return a-b
+print(sub(6,3))
+print(sub(3,6))
 
-# def my_func():
-#     print('hello')#TypeError: my_func() takes 0 positional arguments but 1 was given
 
-# my_func('avinash')
 
-def my_fun(name,age=22):
-    print('hello', name,age)
+# Unlike positional arguments the output remains same in keyword arguemnts
+def sub(a,b):
+    return a-b
+print(sub(a=6,b=3))
+print(sub(b=3,a=6))
 
-my_fun('avinash',)
-my_fun('ramesh',)
-my_fun('mahesh',)
+#Default arguments take default value if we don't provide the value
+
+def add(a,b,c=10,d=14):
+    return a+b+c+d
+
+print(add(1,2,3,5))
+print(add(1,2)) # It takes the default value only when if we don't provide
+print(add(1,2,3))
+
+#  Variable length arguments :
+#It returns all the remaining values as a tuple
+
+def cal(a,b,*c):
+    return a,b,c
+
+print(cal(1,2,3)) # c value is always return as a tuple even if it is single
+print(cal(1,2,3,4,5,6)) # c value takes all the remaining values 
+# print(cal(1)) # Typeerror
+print(cal(1,2)) # Here c becomes an empty tuple
+
+
+
+
 
