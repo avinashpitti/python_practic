de6da85f08bd55c3a84c65d8cd3608f6cb7d9162
avinashpitti <avinash87129@gmail.com>
2026-01-06 17:23:56 +0530

    demonstrate greet() using print,return,f-string and loop

--- practice.py
+++ practice.py
@@ -1,5 +1,58 @@
-def user_details(**info):
-    for key,value in info.items():
-        print(key,':',value)
-user_details(name='avinash',age=22,city='bengaluru')        
-    
+# def greet(name):
+#     print('hello', name)
+
+# greet('avinash')
+# greet('gowtham')
+# greet('harsha')
+# greet('mahesh')
+
+# print('***************')
+
+# def greet(name):
+#     return 'hello'+' '+name
+
+# print(greet('avinash'))
+# print(greet('gowtham'))
+# print(greet('harsha'))
+# print(greet('mahesh'))
+
+# print('***************')
+
+
+# def greet(name):
+#     return f'hello {name}'
+# msg1=greet('avinash')
+# msg2=greet('gowtham')
+# msg3=greet('harsha')
+# msg4=greet('mahesh')
+# print(msg1)
+# print(msg2)
+# print(msg3)
+# print(msg4)
+
+# print('***************')
+
+# names=['avinash','gowtham','harsha','mahesh']
+
+# for name in names:
+#     msg=greet(name)
+#     print(msg)
+
+# print('***************')
+def greet(names):
+    return f'hello{name}'
+
+names=['avinash','gowtham','harsha','mahesh']
+
+messages=[]
+for name in names:
+        messages.append(greet(name))
+print(messages)
+
+
+
+
+
+
+
+
