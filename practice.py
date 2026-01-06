fda8d9e291f509576daac8fedcb022b4d858c735
avinashpitti <avinash87129@gmail.com>
2026-01-06 18:31:07 +0530

    practice: demonstrate even/odd and prime number check

--- practice.py
+++ practice.py
@@ -1,39 +1,51 @@
-def outer():
-    print("outer functon")
-
-    def inner():
-        print("inner function")
-    inner()
-outer()
-
-print('**************************')
-
-def outer(name):
-    print('outer function', name)
-
-    def inner(name):
-        print('inner function', name)
-    inner('avinash')
-outer('avinash')
-
-print('**************************')
+# even or odd
 
 
-def outer():
-    print('outer function')
+def even_odd(num):
+    if num%2==0:
+        print('even')
+    else:
+        print('odd')
 
-    def inner(name):
-        print('inner function', name)
-    inner('avinash')
-outer()
+even_odd(21)
+even_odd(28)
+even_odd(0)
+even_odd(-34)
+even_odd(-43)
 
-print('**************************')
+print('******************')
 
-def outer(name):
-    print("outer function",name)
+def evenodd(num):
+    if num%2==0:
+        return 'even'
+    else:
+        return 'odd'
+print(evenodd(22))
+print(evenodd(33))
+print(evenodd(-12))
+print(evenodd(-34))
+print(evenodd(0))
 
-    def inner():
-        print("inner function")
-    inner()
-outer('avinash')
+
+print('******************')
+
+#prime
+
+def is_prime(n):
+    if n > 1 :
+        for i in range(2,int(n**0.5)+1):
+            if n % i ==0:
+                print('Not a prime')
+                break
+        else:
+            print('prime')
+    else:
+        print('Not a prime')
+is_prime(21)
+is_prime(23)
+is_prime(31)
+
+
+
+
 
