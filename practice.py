ebefe3e5b60942e06ece26404e1c69ee440eace6
avinashpitti <avinash87129@gmail.com>
2026-01-06 17:41:10 +0530

    practice: add and sub functions using return and f-strings

--- practice.py
+++ practice.py
@@ -1,58 +1,12 @@
-# def greet(name):
-#     print('hello', name)
+def add(a,b):
+    return a+b
+print(add(5,8))
+print((f'The sum of numbers is {add(4,2)}'))
 
-# greet('avinash')
-# greet('gowtham')
-# greet('harsha')
-# greet('mahesh')
+def sub(a,b):
+    return a-b
 
-# print('***************')
-
-# def greet(name):
-#     return 'hello'+' '+name
-
-# print(greet('avinash'))
-# print(greet('gowtham'))
-# print(greet('harsha'))
-# print(greet('mahesh'))
-
-# print('***************')
-
-
-# def greet(name):
-#     return f'hello {name}'
-# msg1=greet('avinash')
-# msg2=greet('gowtham')
-# msg3=greet('harsha')
-# msg4=greet('mahesh')
-# print(msg1)
-# print(msg2)
-# print(msg3)
-# print(msg4)
-
-# print('***************')
-
-# names=['avinash','gowtham','harsha','mahesh']
-
-# for name in names:
-#     msg=greet(name)
-#     print(msg)
-
-# print('***************')
-def greet(names):
-    return f'hello{name}'
-
-names=['avinash','gowtham','harsha','mahesh']
-
-messages=[]
-for name in names:
-        messages.append(greet(name))
-print(messages)
-
-
-
-
-
-
-
-
+result1=sub(10,5)
+result2=sub(5,10)
+print(f'The difference of two values is {result1}')
+print(f'The difference of two values is {result2}')
