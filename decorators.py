d135c3eb0da029271f929121864a817ae5751b35
avinashpitti <avinash87129@gmail.com>
2026-01-08 19:32:02 +0530

    passing a function as argument and returning a function(closure)

--- decorators.py
+++ decorators.py
@@ -1,32 +1,15 @@
-def greet():
-    print('hello')
-x=greet
-x()
-
-def greeting():
-    return 'hello,guys'
-x=greeting
-print(x())
-
-
-
-def outer():
-    text_outer = 'This is an outer function.'
-    
-    def inner():
-        return 'This is an inner function.'
-    
-    text_inner = inner()
-    return text_outer, text_inner # Returning two values
-
-y, z = outer() # Unpacking the two values
-print(y)
-print(z)
-
 def say_hello():
     print("Hello!")
 
 def execute(func):
     func()
 
-execute(say_hello)
+
+def outer():
+    def inner():
+        print('Inner function executed')
+
+    return inner
+
+result=outer()
+result()
