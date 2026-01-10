c9bf694565a5587bcce2c326cf0596a526c15ad0
avinashpitti <avinash87129@gmail.com>
2026-01-10 17:53:00 +0530

    finding odd_even and for loop with break and continue

--- practice.py
+++ practice.py
@@ -1,63 +1,18 @@
-#1
+#6
 
-name='avinash'
-age=22
-height=1.56
-student=True
+num=int(input('Enter a number: '))
 
-print(name,age,height,student)
+if num % 2 ==0:
+    print('Even number')
+else:
+    print('Odd number')
 
-#2
+#7
 
-a=10
-b=3
-
-sum=a+b
-difference=a-b
-multiplication=a*b
-division=a/b
-remainder=a%b
-
-print(sum)
-print(difference)
-print(multiplication)
-print(division)
-print(remainder)
-
-#3
-
-fruits=['orange','banana','apple','guava','papaya']
-
-print(fruits[0])
-print(fruits[-1])
-fruits.append('kiwi')
-print(fruits)
-fruits.remove('banana')
-print(fruits)
-
-#4
-
-numbers=(1,2,3)
-
-nums=set(numbers)
-
-print(f'nums before adding a number : {nums}')
-
-nums.add(4)
-print(f'nums after adding a number : {nums}')
-
-my_details={
-    'name':'Avinash',
-    'age': 22,
-    'course':'python'
-}
-
-print(f"my course : {my_details['course']}")
-
-my_details['age']=23
-
-print(f'my updated age : {my_details['age']}')
-
-my_details['city']='bangalore'
-
-print(my_details)
+for i in range(1,11):
+     
+    if i==5:
+        continue
+    if i==8:
+        break
+    print(i)  
