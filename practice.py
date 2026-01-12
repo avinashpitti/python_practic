a043cdfdbe1a4487f68810068fb8dac6d0f41893
avinashpitti <avinash87129@gmail.com>
2026-01-12 09:42:47 +0530

    finding min_max in a single functionn

--- practice.py
+++ practice.py
@@ -1,13 +1,15 @@
-# write a function that takes a list of numbers and returns the largest
+# write a function that takes a list of numbers and returns the second largest
 
-nums=[2,57,124,555,33,20]
+nums=[112,57,124,555,33,20]
 
-    
-def largest_nums(nums):
+
+def min_max(nums):
     large=nums[0]
+    small=nums[0]
     for i in nums:
-        if i > large:
+        if i > large :
             large=i
-    return large
-
-print(largest_nums(nums))
+        if i < small :
+            small=i
+    return large,small
+print(min_max(nums))
