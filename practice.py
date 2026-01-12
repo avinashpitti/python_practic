63be5a460b6b96f01376609c4652c90c5eb38ef9
avinashpitti <avinash87129@gmail.com>
2026-01-12 08:50:13 +0530

    finding the largest number from the list

--- practice.py
+++ practice.py
@@ -1,15 +1,13 @@
-# write a function that takes a list of numbers and returns the smallest
+# write a function that takes a list of numbers and returns the largest
 
-nums=[9,57,124,555,3,20]
-
-def smallest_num(nums):
-    small=nums[0]
-
-    for i in nums:
-        if i < small:
-            small=i
-
-    return small
-print(smallest_num(nums))
+nums=[2,57,124,555,33,20]
 
     
+def largest_nums(nums):
+    large=nums[0]
+    for i in nums:
+        if i > large:
+            large=i
+    return large
+
+print(largest_nums(nums))
