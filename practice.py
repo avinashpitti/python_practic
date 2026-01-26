a65f8218f1144ec4c55e331e99811a4304ba20d2
avinashpitti <avinash87129@gmail.com>
2026-01-26 18:57:07 +0530

    append and remove from a list

--- practice.py
+++ practice.py
@@ -1,4 +1,8 @@
-s='python'
+nums = [10, 20, 30, 40, 50]
 
-print(f'The reverse of "{s}" is "{s[: :-1]}"')
-print(f'The length of "{s}" is {len(s)}')
+nums.append(60)
+
+nums.remove(30)
+
+nums.pop(4)
+print(nums)
