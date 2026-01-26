8262bdd80b4e1e66e89251145c2088ca7e62064a
avinashpitti <avinash87129@gmail.com>
2026-01-26 18:41:11 +0530

    arithmetic operations

--- practice.py
+++ practice.py
@@ -1,6 +1,7 @@
-num=int(input("Enter a number : "))
+x=int(input("Enter first number : "))
+y=int(input("Enter second number : "))
 
-if num % 2 ==0:
-    print(f'{num} is an even number')
-else:
-    print(f'{num} is an odd number')
+print(f'The sum of {x} and {y} is {x+y}')
+print(f'The difference of {x} and {y} is {x-y}')
+print(f'The product of {x} and {y} is {x*y}')
+print(f'The division of {x} and {y} is {x/y}')
