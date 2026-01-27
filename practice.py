19f2d1f652a0777d2c2a0ddd59b08cf0d63e19f4
avinashpitti <avinash87129@gmail.com>
2026-01-27 09:46:03 +0530

    functions with return statement

--- practice.py
+++ practice.py
@@ -1,6 +1,8 @@
-def greet(name):
-    print(f'hello {name}')
-greet('avinash')
-greet('srinivas')
-greet('chandu')
+def add(a,b):
+    return a+b
+print(f'The sum of numbers is {add(2,3)}')
 
+def sub(a,b):
+    return a-b
+diff=sub(4,1)
+print(f'The difference of numbers is {diff}')
