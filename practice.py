b28c19d34b6403edafe858aad35e257ef583b2e4
avinashpitti <avinash87129@gmail.com>
2026-01-29 10:48:03 +0530

    practice finding number of lines  and count number of characters

--- practice.py
+++ practice.py
@@ -1,7 +1,52 @@
-def min_max(a,b):
-    return max(a,b),min(a,b)
+from importlib.metadata import files
+with open('notes.txt','w') as f:
+    print(f.write('This is line 1\n'))
+    print(f.write('This is line 2\n'))
+    print(f.write('This is line 3\n'))
+    print(f.write('This is line 4\n'))
+    print(f.write('This is line 5\n'))
 
-maximum,mininum=min_max(1,5)
+with open('notes.txt','r') as f:
+    
+    data=f.read()
+    print(data)
 
-print(f'maximum value is {maximum}')
-print(f'minimum value is {mininum}')
+with open('notes.txt','r') as f :
+    data =f.readline()
+    print(data)
+
+with open('notes.txt','w') as f :
+    data=f.write('my name is avinash\n')
+    data=f.write('I am 21 years old\n')
+
+with open('notes.txt','a')as f:
+    data=f.write('Today date is 29-jan-2026')
+
+with open('notes.txt','r') as f :
+    data=f.read()
+    print('Number of characters :',len(data))
+
+with open('notes.txt','r') as f :
+    lines=f.readlines()  
+    print('Number of lines : ',len(lines))
+
+count=0
+with open('notes.txt','r') as f :
+    for line in f :
+        count+=1
+print("Number of lines :",count)
+
+
+# Task
+# Invoke restapis(request)
+# read json file(products.json)
+# write into new pdf files
+
+# task1
+# products.json
+# readwrite.py
+# prod.pdf
+
+
+
+
