9e91365719be72737571c52de6ce4cc08494a594
avinashpitti <avinash87129@gmail.com>
2026-02-03 09:41:09 +0530

    finding odd_even numbers in different methods by using conditionals and functions

--- practice.py
+++ practice.py
@@ -1,52 +1,33 @@
-from importlib.metadata import files
-with open('notes.txt','w') as f:
-    print(f.write('This is line 1\n'))
-    print(f.write('This is line 2\n'))
-    print(f.write('This is line 3\n'))
-    print(f.write('This is line 4\n'))
-    print(f.write('This is line 5\n'))
+# write a program to check whether a number is even or odd
+# n=int(input('Enter a number : '))
 
-with open('notes.txt','r') as f:
-    
-    data=f.read()
-    print(data)
+# if n % 2 ==0 :
+#     print(f'{n} is an even number')
+# else:
+#     print(f'{n} is an odd number')
 
-with open('notes.txt','r') as f :
-    data =f.readline()
-    print(data)
-
-with open('notes.txt','w') as f :
-    data=f.write('my name is avinash\n')
-    data=f.write('I am 21 years old\n')
-
-with open('notes.txt','a')as f:
-    data=f.write('Today date is 29-jan-2026')
-
-with open('notes.txt','r') as f :
-    data=f.read()
-    print('Number of characters :',len(data))
-
-with open('notes.txt','r') as f :
-    lines=f.readlines()  
-    print('Number of lines : ',len(lines))
-
-count=0
-with open('notes.txt','r') as f :
-    for line in f :
-        count+=1
-print("Number of lines :",count)
+# n=int(input('Enter a number : '))
+# def odd_even(n):
+#     if n % 2 ==0 :
+#         return f"{n} is an even number"
+#     else :
+#         return f"{n} is an odd number"
+# print(odd_even(n))
 
 
-# Task
-# Invoke restapis(request)
-# read json file(products.json)
-# write into new pdf files
+# n=int(input('Enter a number : '))
+# def odd_even(n):
+#     return 'even' if n % 2==0 else 'odd'
+# print(f'{n} is {odd_even(n)}')
 
-# task1
-# products.json
-# readwrite.py
-# prod.pdf
 
+n=int(input('Enter a number: '))
+def odd_even(n):
+    if n % 2 ==0 :
+        print(f'{n} is an even number')
+    else :
+        print(f'{n} is an odd number')
+odd_even(n)
 
 
 
