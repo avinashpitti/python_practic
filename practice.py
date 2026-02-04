bab7086a75dd6836541c86ecd4961c54524ccd29
avinashpitti <avinash87129@gmail.com>
2026-02-04 11:40:34 +0530

    tuples in python

--- practice.py
+++ practice.py
@@ -1,8 +1,18 @@
-# A list is an ordered,mutable collection that allows duplicates
-# we can access values by using indexing
-info=[1,2,3,3,4,3,True,'avi']
-print(info) # It takes heterogeneous data
-print(type(info)) # <class list>
-info[0]=99
-print(info[0]) #99
-print(f'The length of data is {len(info)}')
+# # A list is an ordered,mutable collection that allows duplicates
+# # we can access values by using indexing
+# info=[1,2,3,3,4,3,True,'avi']
+# print(info) # It takes heterogeneous data
+# print(type(info)) # <class list>
+# info[0]=99
+# print(info[0]) #99
+# print(f'The length of data is {len(info)}')
+
+# Tuple : A tuple is an ordered, immutable collection that allows duplicates
+# Faster than list
+
+data=(1,2,3,'akash','siri','alexa',False,5.7)
+print(data)
+print(type(data))
+# data[0]=656 # Tuple object does not support item assignment
+# print(data[0])
+
